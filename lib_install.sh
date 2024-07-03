d3f9973b8881981a4c3e621d9ca558f5f556af10
aimlrl <mlresearch.axis@gmail.com>
2024-07-03 22:50:21 +0530

    first commit of first project of BADCCE5103

---
+++ lib_install.sh
@@ -0,0 +1,7 @@
+pip3 install kaggle
+pip3 install typing-extensions<4.6.0,>=3.6.6
+pip3 install tensorflow==2.13.0
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip3 install pandas
+pip3 install numpy
+pip3 install matplotlib
